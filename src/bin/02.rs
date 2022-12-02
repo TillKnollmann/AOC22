eb8eadd402c95d425103b0da434612e84258bb40
Till Knollmann <tillk@mail.upb.de>
2022-12-02 07:32:22 +0100

    Add comments for day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -2,24 +2,29 @@ use std::vec;
 
 use itertools::Itertools;
 
+/// Rock, paper, scissors game.
+/// Rock has score 1, paper score 2, scissors score 3
+/// Loosing increases score by 0, draw by 3, win by 6
+
+/// Returns the score for part 1
 fn get_score_pt_1(A: &str, B: &str) -> u32 {
     match A {
         "A" => match B {
-            "X" => 1 + 3,
-            "Y" => 2 + 6,
-            "Z" => 3 + 0,
+            "X" => 1 + 3, // rock vs. rock means draw
+            "Y" => 2 + 6, // rock vs. paper means win
+            "Z" => 3 + 0, // rock vs. scissors means loose
             _ => {println!("Action unclear: {}", B); return 0;},
         },
         "B" => match B {
-            "X" => 1 + 0,
-            "Y" => 2 + 3,
-            "Z" => 3 + 6,
+            "X" => 1 + 0, // paper vs. rock means loose
+            "Y" => 2 + 3, // paper vs. paper means draw
+            "Z" => 3 + 6, // paper vs. scissors means win
             _ => {println!("Action unclear: {}", B); return 0;},
         },
         "C" => match B {
-            "X" => 1 + 6,
-            "Y" => 2 + 0,
-            "Z" => 3 + 3,
+            "X" => 1 + 6, // scissors vs. rock means win
+            "Y" => 2 + 0, // scissors vs. paper means loose
+            "Z" => 3 + 3, // scissors vs. scissors means draw
             _ => {println!("Action unclear: {}", B); return 0;},
         },
         _ => {println!("Action unclear: {}", A); return 0;},
@@ -31,34 +36,39 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut scores: Vec<u32> = vec![];
 
     for line in input.split("\n") {
+        // cleanup line
         let clean_line = line.replace("\n", "").replace("\r", "");
         if clean_line.len()>0 {
+            // get actions
             let actions: Vec<&str> = clean_line.split(" ").collect_vec();
+            // add score
             scores.push(get_score_pt_1(actions[0], actions[1]));
         }
     }
 
+    // return sum
     Some(scores.iter().sum())
 }
 
+/// Returns the score for part 2
 fn get_score_pt_2(A: &str, B: &str) -> u32 {
     match A {
         "A" => match B {
-            "X" => 3 + 0,
-            "Y" => 1 + 3,
-            "Z" => 2 + 6,
+            "X" => 3 + 0, // loose against rock means scissors
+            "Y" => 1 + 3, // draw against rock means rock
+            "Z" => 2 + 6, // win against rock means paper
             _ => {println!("Action unclear: {}", B); return 0;},
         },
         "B" => match B {
-            "X" => 1 + 0,
-            "Y" => 2 + 3,
-            "Z" => 3 + 6,
+            "X" => 1 + 0, // loose against paper means rock
+            "Y" => 2 + 3, // draw against paper means paper
+            "Z" => 3 + 6, // win against paper means scissors
             _ => {println!("Action unclear: {}", B); return 0;},
         },
         "C" => match B {
-            "X" => 2 + 0,
-            "Y" => 3 + 3,
-            "Z" => 1 + 6,
+            "X" => 2 + 0, // loose against scissors means paper
+            "Y" => 3 + 3, // draw against scissors means scissors
+            "Z" => 1 + 6, // win against scissors means rock
             _ => {println!("Action unclear: {}", B); return 0;},
         },
         _ => {println!("Action unclear: {}", A); return 0;},
@@ -66,16 +76,21 @@ fn get_score_pt_2(A: &str, B: &str) -> u32 {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+
     let mut scores: Vec<u32> = vec![];
 
     for line in input.split("\n") {
+        // cleanup line
         let clean_line = line.replace("\n", "").replace("\r", "");
         if clean_line.len()>0 {
+            // get actions
             let actions: Vec<&str> = clean_line.split(" ").collect_vec();
+            // add score
             scores.push(get_score_pt_2(actions[0], actions[1]));
         }
     }
 
+    // return sum
     Some(scores.iter().sum())
 }
 
